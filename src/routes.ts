6c42ea88da85de5d9685165767ca637eeebc60a4
mrgenco <mrgenco@gmail.com>
2022-04-02 13:06:06 +0300

    metrics and swagger docs are removed

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Express, Request, Response } from "express";
+
+/* Controllers */
 import {
   createProductHandler,
   getProductHandler,
@@ -11,8 +13,8 @@ import {
   deleteSessionHandler,
 } from "./controller/session.controller";
 import { createUserHandler } from "./controller/user.controller";
-import requireUser from "./middleware/requireUser";
-import validateResource from "./middleware/validateResource";
+
+/* Schemas */
 import {
   createProductSchema,
   deleteProductSchema,
@@ -22,45 +24,14 @@ import {
 import { createSessionSchema } from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
 
+/* Middlewares */
+import requireUser from "./middleware/requireUser";
+import validateResource from "./middleware/validateResource";
+
 function routes(app: Express) {
-  /**
-   * @openapi
-   * /healthcheck:
-   *  get:
-   *     tags:
-   *     - Healthcheck
-   *     description: Responds if the app is up and running
-   *     responses:
-   *       200:
-   *         description: App is up and running
-   */
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
-  /**
-   * @openapi
-   * '/api/users':
-   *  post:
-   *     tags:
-   *     - User
-   *     summary: Register a user
-   *     requestBody:
-   *      required: true
-   *      content:
-   *        application/json:
-   *           schema:
-   *              $ref: '#/components/schemas/CreateUserInput'
-   *     responses:
-   *      200:
-   *        description: Success
-   *        content:
-   *          application/json:
-   *            schema:
-   *              $ref: '#/components/schemas/CreateUserResponse'
-   *      409:
-   *        description: Conflict
-   *      400:
-   *        description: Bad request
-   */
+  /* User Endpoints */
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
 
   app.post(
@@ -73,34 +44,13 @@ function routes(app: Express) {
 
   app.delete("/api/sessions", requireUser, deleteSessionHandler);
 
+  /* Product Endpoints */
   app.post(
     "/api/products",
     [requireUser, validateResource(createProductSchema)],
     createProductHandler
   );
 
-  /**
-   * @openapi
-   * '/api/products/{productId}':
-   *  get:
-   *     tags:
-   *     - Products
-   *     summary: Get a single product by the productId
-   *     parameters:
-   *      - name: productId
-   *        in: path
-   *        description: The id of the product
-   *        required: true
-   *     responses:
-   *       200:
-   *         description: Success
-   *         content:
-   *          application/json:
-   *           schema:
-   *              $ref: '#/components/schema/Product'
-   *       404:
-   *         description: Product not found
-   */
   app.put(
     "/api/products/:productId",
     [requireUser, validateResource(updateProductSchema)],
